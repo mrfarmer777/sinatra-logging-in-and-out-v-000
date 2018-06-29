e38e0c4424b716136ae0d904c67671c29bb7a699
Matt Farmer <mrfarmer777@gmail.com>
2018-06-29 18:00:41 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,10 +1,10 @@
 class Helpers
-  def current_user(session)
+  def self.current_user(session)
     @user=User.find(session[:user_id]) #?Not sure if the session hash can be passed straight in here
     @user
   end
 
-  def is_logged_in?(session)
+  def self.is_logged_in?(session)
     session.key?(:user_id)
   end
 
